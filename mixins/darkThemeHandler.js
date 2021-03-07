649382ad870daaad80a4578cc0cde2614ae81732
Rashidov Shoxrux <u2b3ki@gmail.com>
2021-03-07 14:48:52 +0300

    Eslint xatolari tuzaltildi

--- mixins/darkThemeHandler.js
+++ mixins/darkThemeHandler.js
@@ -3,25 +3,25 @@ export default {
     return {
       darkTheme: false,
       ignoreForcedThemes: false,
-    };
+    }
   },
 
   mounted() {
     if (!this.xenon.disableDarkTheme) {
-      if (!this.xenon.defaultDarkTheme && !this.getItem("dark-theme")) {
-        this.setItem("dark-theme", true);
+      if (!this.xenon.defaultDarkTheme && !this.getItem('dark-theme')) {
+        this.setItem('dark-theme', true)
       }
-      if (this.getItem("dark-theme")) {
-        this.darkTheme = true;
-        this.toggleDarkTheme();
+      if (this.getItem('dark-theme')) {
+        this.darkTheme = true
+        this.toggleDarkTheme()
       }
     }
 
     if (
-      !this.xenon.disableThemeIgnore &&
-      this.getItem("ignore-forced-themes")
+      !this.xenon.disableThemeIgnore
+      && this.getItem('ignore-forced-themes')
     ) {
-      this.ignoreForcedThemes = true;
+      this.ignoreForcedThemes = true
     }
   },
 
@@ -33,7 +33,7 @@ export default {
      * @returns string
      */
     setItem(key, value) {
-      return localStorage.setItem(key, value);
+      return localStorage.setItem(key, value)
     },
     /**
      *
@@ -41,25 +41,27 @@ export default {
      * @returns string
      */
     getItem(key) {
-      return localStorage.getItem(key);
+      return localStorage.getItem(key)
     },
+    // eslint-disable-next-line consistent-return
     toggleDarkTheme() {
       if (this.darkTheme) {
-        document.body.classList.add("xenon-theme-dark");
-        return this.setItem("dark-theme", true);
+        document.body.classList.add('xenon-theme-dark')
+        return this.setItem('dark-theme', true)
       }
 
-      document.body.classList.remove("xenon-theme-dark");
-      this.setItem("dark-theme", false);
+      document.body.classList.remove('xenon-theme-dark')
+      this.setItem('dark-theme', false)
     },
 
+    // eslint-disable-next-line consistent-return
     toggleForcedThemes() {
       if (this.ignoreForcedThemes) {
-        this.setTheme(this.getItem("color-theme"));
-        return this.setItem("ignore-forced-themes", true);
+        this.setTheme(this.getItem('color-theme'))
+        return this.setItem('ignore-forced-themes', true)
       }
 
-      localStorage.removeItem("ignore-forced-themes");
+      localStorage.removeItem('ignore-forced-themes')
     },
   },
-};
+}
