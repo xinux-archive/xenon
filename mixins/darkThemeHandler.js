d8b2c7af95a36b4faaccc60e9b34033ad70cbaea
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2021-03-11 17:50:53 +0500

    formatting

--- mixins/darkThemeHandler.js
+++ mixins/darkThemeHandler.js
@@ -1,67 +1,67 @@
 export default {
-  data() {
-    return {
-      darkTheme: false,
-      ignoreForcedThemes: false,
-    }
-  },
+	data() {
+		return {
+			darkTheme: false,
+			ignoreForcedThemes: false,
+		};
+	},
 
-  mounted() {
-    if (!this.xenon.disableDarkTheme) {
-      if (!this.xenon.defaultDarkTheme && !this.getItem('dark-theme')) {
-        this.setItem('dark-theme', true)
-      }
-      if (this.getItem('dark-theme')) {
-        this.darkTheme = true
-        this.toggleDarkTheme()
-      }
-    }
+	mounted() {
+		if (!this.xenon.disableDarkTheme) {
+			if (!this.xenon.defaultDarkTheme && !this.getItem('dark-theme')) {
+				this.setItem('dark-theme', true);
+			}
+			if (this.getItem('dark-theme')) {
+				this.darkTheme = true;
+				this.toggleDarkTheme();
+			}
+		}
 
-    if (
-      !this.xenon.disableThemeIgnore
+		if (
+			!this.xenon.disableThemeIgnore
       && this.getItem('ignore-forced-themes')
-    ) {
-      this.ignoreForcedThemes = true
-    }
-  },
+		) {
+			this.ignoreForcedThemes = true;
+		}
+	},
 
-  methods: {
-    /**
+	methods: {
+		/**
      *
      * @param {*} key
      * @param {*} value
      * @returns string
      */
-    setItem(key, value) {
-      return localStorage.setItem(key, value)
-    },
-    /**
+		setItem(key, value) {
+			return localStorage.setItem(key, value);
+		},
+		/**
      *
      * @param {*} key
      * @returns string
      */
-    getItem(key) {
-      return localStorage.getItem(key)
-    },
-    // eslint-disable-next-line consistent-return
-    toggleDarkTheme() {
-      if (this.darkTheme) {
-        document.body.classList.add('xenon-theme-dark')
-        return this.setItem('dark-theme', true)
-      }
+		getItem(key) {
+			return localStorage.getItem(key);
+		},
+		// eslint-disable-next-line consistent-return
+		toggleDarkTheme() {
+			if (this.darkTheme) {
+				document.body.classList.add('xenon-theme-dark');
+				return this.setItem('dark-theme', true);
+			}
 
-      document.body.classList.remove('xenon-theme-dark')
-      this.setItem('dark-theme', false)
-    },
+			document.body.classList.remove('xenon-theme-dark');
+			this.setItem('dark-theme', false);
+		},
 
-    // eslint-disable-next-line consistent-return
-    toggleForcedThemes() {
-      if (this.ignoreForcedThemes) {
-        this.setTheme(this.getItem('color-theme'))
-        return this.setItem('ignore-forced-themes', true)
-      }
+		// eslint-disable-next-line consistent-return
+		toggleForcedThemes() {
+			if (this.ignoreForcedThemes) {
+				this.setTheme(this.getItem('color-theme'));
+				return this.setItem('ignore-forced-themes', true);
+			}
 
-      localStorage.removeItem('ignore-forced-themes')
-    },
-  },
-}
+			localStorage.removeItem('ignore-forced-themes');
+		},
+	},
+};
